8cb6f646034c85fe139353c445c1377042a3a05f
c0a22063e0 <c0a22063e0@edu.teu.ac.jp>
2023-09-26 18:22:37 +0900

    演習最新

--- pygame_image.py
+++ pygame_image.py
@@ -7,22 +7,27 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_image2 = pg.transform.rotozoom(kk_img, 10, 1.0)
     kk_image_list = [kk_img, kk_image2]
+    bg_image_list = [bg_img, bg_img2]*2
     tmr = 0
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%1600
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
-        screen.blit(kk_image_list[tmr//50%2], [300, 200])
+        x = tmr%3200
+        for i in range(2):
+            screen.blit(bg_image_list[i],[3200*i-x,0])
+
+        #screen.blit(bg_img, [-x, 0])
+        #screen.blit(bg_img2, [1600-x, 0])
+        screen.blit(kk_image_list[tmr//200%2], [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(100) 
+        clock.tick(1000) 
 
 
 if __name__ == "__main__":
